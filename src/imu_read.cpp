012125db039b755e3e8f3c3844a93c41643e04c2
srujanpanuganti <panuganti.srujan@gmail.com>
2020-06-24 22:14:44 -0400

    edited imur_read 	modified:   src/imu_read.cpp

--- src/imu_read.cpp
+++ src/imu_read.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv){
 
     ros::NodeHandle nh;
 
-    ros::Publisher chatter_pub = nh.advertise<sensor_msgs::Imu>("imu/raw_data", 1000);
+    ros::Publisher chatter_pub = nh.advertise<sensor_msgs::Imu>("imu/raw_data", 4000);
 
     rosbag::Bag Imu_bag("Img_bag.bag", rosbag::bagmode::Write);
 
@@ -32,6 +32,7 @@ int main(int argc, char **argv){
     mySerial.open();
 
     int count = 0;
+    int num = 0;
 
 
     while(ros::ok()){
@@ -42,35 +43,42 @@ int main(int argc, char **argv){
 
         std::string line;
         line = mySerial.readline(1024,"\n");
+//        line = mySerial.readline();
+
         mySerial.flush();
 
-        ROS_INFO("%s", line.c_str());
+        if(num >10) {
 
-        char *pch;
-        pch = std::strtok(&line[0], " ");
-        std::string number;
-        double data[6] = {0};
-        int count = 0;
-        while(pch != NULL)
-        {
-            number = pch;
-            data[count] = atof(number.c_str());
-            pch = std::strtok(NULL, " ");
-            count++;
+            ROS_INFO("%s", line.c_str());
+
+            char *pch;
+            pch = std::strtok(&line[0], " ");
+            std::string number;
+            double data[6] = {0};
+            int count = 0;
+            while (pch != NULL) {
+                number = pch;
+                data[count] = atof(number.c_str());
+                pch = std::strtok(NULL, " ");
+                count++;
+            }
+
+            float angvel_conversion = .005 * 3.1415 / 180.0;
+            float linacc_conversion = .00025 * 9.8;
+
+            imu_msg.angular_velocity.x = data[0] * angvel_conversion;
+            imu_msg.angular_velocity.y = data[1] * angvel_conversion;
+            imu_msg.angular_velocity.z = data[2] * angvel_conversion;
+            imu_msg.linear_acceleration.x = data[3] * linacc_conversion;
+            imu_msg.linear_acceleration.y = data[4] * linacc_conversion;
+            imu_msg.linear_acceleration.z = data[5] * linacc_conversion;
+
+//            ROS_INFO_STREAM();
+            chatter_pub.publish(imu_msg);
+            Imu_bag.write("Imu", ros::Time::now(), imu_msg);
         }
 
-        float angvel_conversion = .005*3.1415/180.0;
-        float linacc_conversion = .00025*9.8;
-
-        imu_msg.angular_velocity.x = data[0]*angvel_conversion;
-        imu_msg.angular_velocity.y = data[1]*angvel_conversion;
-        imu_msg.angular_velocity.z = data[2]*angvel_conversion;
-        imu_msg.linear_acceleration.x = data[3]*linacc_conversion;
-        imu_msg.linear_acceleration.y = data[4]*linacc_conversion;
-        imu_msg.linear_acceleration.z = data[5]*linacc_conversion;
-
-        chatter_pub.publish(imu_msg);
-        Imu_bag.write("Imu", ros::Time::now(), imu_msg);
+        num++;
 
         ros::spinOnce();
 
