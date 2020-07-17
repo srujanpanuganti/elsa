d360c064586503699db6419e46ec6a7e984df91d
srujanpanuganti <panuganti.srujan@gmail.com>
2020-07-16 22:58:15 -0400

    new file:   ros_arduino/README.md 	new file:   ros_arduino/ros_arduino_bridge/CMakeLists.txt 	new file:   ros_arduino/ros_arduino_bridge/package.xml 	new file:   ros_arduino/ros_arduino_firmware/CMakeLists.txt 	new file:   ros_arduino/ros_arduino_firmware/package.xml 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/MegaRobogaiaPololu/MegaRobogaiaPololu.ino 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/MegaRobogaiaPololu/commands.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/MegaRobogaiaPololu/diff_controller.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/MegaRobogaiaPololu/sensors.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/MegaRobogaiaPololu/servos.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/ROSArduinoBridge.ino 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/commands.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/diff_controller.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.ino 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.ino 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/sensors.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/servos.h 	new file:   ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/servos.ino 	new file:   ros_arduino/ros_arduino_msgs/CMakeLists.txt 	new file:   ros_arduino/ros_arduino_msgs/msg/Analog.msg 	new file:   ros_arduino/ros_arduino_msgs/msg/AnalogFloat.msg 	new file:   ros_arduino/ros_arduino_msgs/msg/ArduinoConstants.msg 	new file:   ros_arduino/ros_arduino_msgs/msg/Digital.msg 	new file:   ros_arduino/ros_arduino_msgs/msg/SensorState.msg 	new file:   ros_arduino/ros_arduino_msgs/package.xml 	new file:   ros_arduino/ros_arduino_msgs/srv/AnalogRead.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/AnalogWrite.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/DigitalRead.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/DigitalSetDirection.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/DigitalWrite.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/ServoRead.srv 	new file:   ros_arduino/ros_arduino_msgs/srv/ServoWrite.srv 	new file:   ros_arduino/ros_arduino_python/CMakeLists.txt 	new file:   ros_arduino/ros_arduino_python/config/arduino_params.yaml 	new file:   ros_arduino/ros_arduino_python/config/arduino_params_modified.yaml 	new file:   ros_arduino/ros_arduino_python/launch/arduino.launch 	new file:   ros_arduino/ros_arduino_python/nodes/arduino_node.py 	new file:   ros_arduino/ros_arduino_python/package.xml 	new file:   ros_arduino/ros_arduino_python/setup.py 	new file:   ros_arduino/ros_arduino_python/src/ros_arduino_python/__init__.py 	new file:   ros_arduino/ros_arduino_python/src/ros_arduino_python/arduino_driver.py 	new file:   ros_arduino/ros_arduino_python/src/ros_arduino_python/arduino_sensors.py 	new file:   ros_arduino/ros_arduino_python/src/ros_arduino_python/base_controller.py 	deleted:    ros_arduino_bridge

---
+++ ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/commands.h
@@ -0,0 +1,24 @@
+/* Define single-letter commands that will be sent by the PC over the
+   serial link.
+*/
+
+#ifndef COMMANDS_H
+
+#define COMMANDS_H
+#define ANALOG_READ    'a'
+#define GET_BAUDRATE   'b'
+#define PIN_MODE       'c'
+#define DIGITAL_READ   'd'
+#define READ_ENCODERS  'e'
+#define MOTOR_SPEEDS   'm'
+#define PING           'p'
+#define RESET_ENCODERS 'r'
+#define SERVO_WRITE    's'
+#define SERVO_READ     't'
+#define UPDATE_PID     'u'
+#define DIGITAL_WRITE  'w'
+#define ANALOG_WRITE   'x'
+#define LEF            0
+#define RIG           1
+
+#endif
---
+++ ros_arduino/ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
@@ -0,0 +1,36 @@
+/***************************************************************
+   Motor driver function definitions - by James Nugen
+   *************************************************************/
+
+#ifdef L298_MOTOR_DRIVER
+  #define RIGHT_MOTOR_BACKWARD 2
+  #define LEFT_MOTOR_BACKWARD  7
+  #define RIGHT_MOTOR_FORWARD  4
+  #define LEFT_MOTOR_FORWARD   8
+  #define RIGHT_MOTOR_ENABLE 5
+  #define LEFT_MOTOR_ENABLE 6
+#endif
+
+void initMotorController();
+void setMotorSpeed(int i, int spd);
+void setMotorSpeeds(int leftSpeed, int rightSpeed);
+
+
+
+
+//RIGHT_MOTOR_BACKWARD 5
+//LEFT_MOTOR_BACKWARD  6
+//RIGHT_MOTOR_FORWARD  9
+//LEFT_MOTOR_FORWARD   10
+//RIGHT_MOTOR_ENABLE 12
+//LEFT_MOTOR_ENABLE 13
+
+
+
+
+//  #define RIGHT_MOTOR_BACKWARD 2
+//  #define LEFT_MOTOR_BACKWARD  7
+//  #define RIGHT_MOTOR_FORWARD  4
+//  #define LEFT_MOTOR_FORWARD   8
+//  #define RIGHT_MOTOR_ENABLE 5
+//  #define LEFT_MOTOR_ENABLE 6
