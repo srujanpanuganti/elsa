98ff28311a0b303301cb53eb300a313e31c78db9
srujanpanuganti <panuganti.srujan@gmail.com>
2020-07-19 00:02:46 -0400

    modified:   elsa/src/tf_broadcaster.cpp

--- elsa/src/tf_broadcaster.cpp
+++ elsa/src/tf_broadcaster.cpp
@@ -18,7 +18,9 @@ int main(int argc, char** argv){
     while(n.ok()){
         broadcaster.sendTransform(
                 tf::StampedTransform(
-                        tf::Transform(tf::Quaternion(0, 0, -0.7071, 0.7071), tf::Vector3(-0.0145, -0.0765, -0.06)),
+//                        tf::Transform(tf::Quaternion(0, 0, 1, 0), tf::Vector3(-0.0145, -0.0765, -0.06)),
+                        tf::Transform(tf::Quaternion(0, 0, 1, 0), tf::Vector3(-0.021, 0.0215, -0.06)),
+
                         ros::Time::now(),"odom", "base_link"));
 //                        ros::Time::now(),"base_link", "odom"));
         r.sleep();
