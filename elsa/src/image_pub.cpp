d500ebe44212969c3f229fadd2e5a55816657b40
srujanpanuganti <panuganti.srujan@gmail.com>
2020-07-22 13:44:55 -0400

    modified:   image_pub.cpp

--- elsa/src/image_pub.cpp
+++ elsa/src/image_pub.cpp
@@ -65,68 +65,68 @@
 //    return 0;
 //}
 
-//#include <ros/ros.h>
-//#include <image_transport/image_transport.h>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <cv_bridge/cv_bridge.h>
-//#include <sstream> // for converting the command line parameter to integer
-//
-//int main(int argc, char** argv)
-//{
-//    // Check if video source has been passed as a parameter
-//    if(argv[1] == NULL) return 1;
-//
-//    ros::init(argc, argv, "image_publisher");
-//    ros::NodeHandle nh;
-//    image_transport::ImageTransport it(nh);
-//    image_transport::Publisher pub = it.advertise("camera/image", 1);
-//
-//    // Convert the passed as command line parameter index for the video device to an integer
-//    std::istringstream video_sourceCmd(argv[1]);
-//    int video_source;
-//    // Check if it is indeed a number
-//    if(!(video_sourceCmd >> video_source)) return 1;
-//
-//    cv::VideoCapture cap(video_source);
-//    // Check if video device can be opened with the given index
-//    if(!cap.isOpened()) return 1;
-//    cv::Mat frame;
-//    sensor_msgs::ImagePtr msg;
-//
-//    ros::Rate loop_rate(5);
-//    while (nh.ok()) {
-//        cap >> frame;
-//        // Check if grabbed frame is actually full with some content
-//        if(!frame.empty()) {
-//            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-//            pub.publish(msg);
-//            cv::waitKey(1);
-//        }
-//
-//        ros::spinOnce();
-//        loop_rate.sleep();
-//    }
-//}
-
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <sstream> // for converting the command line parameter to integer
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+    // Check if video source has been passed as a parameter
+    if(argv[1] == NULL) return 1;
+
     ros::init(argc, argv, "image_pub");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub = it.advertise("camera/image", 1);
-    cv::Mat image = cv::imread("home/pi/elsa_ws/src/elsa/images/tf.png");
-    cv::waitKey(30);
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+
+    // Convert the passed as command line parameter index for the video device to an integer
+    std::istringstream video_sourceCmd(argv[1]);
+    int video_source;
+    // Check if it is indeed a number
+    if(!(video_sourceCmd >> video_source)) return 1;
+
+    cv::VideoCapture cap(video_source);
+    // Check if video device can be opened with the given index
+    if(!cap.isOpened()) return 1;
+    cv::Mat frame;
+    sensor_msgs::ImagePtr msg;
 
     ros::Rate loop_rate(5);
     while (nh.ok()) {
-        pub.publish(msg);
+        cap >> frame;
+        // Check if grabbed frame is actually full with some content
+        if(!frame.empty()) {
+            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            pub.publish(msg);
+            cv::waitKey(1);
+        }
+
         ros::spinOnce();
         loop_rate.sleep();
     }
 }
+
+//#include <ros/ros.h>
+//#include <image_transport/image_transport.h>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <cv_bridge/cv_bridge.h>
+//
+//int main(int argc, char **argv)
+//{
+//    ros::init(argc, argv, "image_pub");
+//    ros::NodeHandle nh;
+//    image_transport::ImageTransport it(nh);
+//    image_transport::Publisher pub = it.advertise("camera/image", 1);
+//    cv::Mat image = cv::imread("home/pi/elsa_ws/src/elsa/images/tf.png");
+//    cv::waitKey(30);
+//    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+//
+//    ros::Rate loop_rate(5);
+//    while (nh.ok()) {
+//        pub.publish(msg);
+//        ros::spinOnce();
+//        loop_rate.sleep();
+//    }
+//}
