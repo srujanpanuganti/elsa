41aedae56dac72ffedca32c82e0e89bf9ec70f5a
srujanpanuganti <panuganti.srujan@gmail.com>
2020-07-22 15:26:38 -0400

    modified:   elsa/src/image_sub.cpp

--- elsa/src/image_sub.cpp
+++ elsa/src/image_sub.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv){
 //    cv::startWindowThread();
 
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("/camera/image", 1, imageCallback);
+    image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
 
     ros::spin();
     cv::destroyWindow("view");
