92c618a44a7b647efced05826fef51f35e0a9d43
srujanpanuganti <panuganti.srujan@gmail.com>
2020-08-02 17:37:03 -0400

    modified:   elsa/src/tf_broadcaster.cpp

--- elsa/src/tf_broadcaster.cpp
+++ elsa/src/tf_broadcaster.cpp
@@ -20,11 +20,15 @@ int main(int argc, char** argv){
                 tf::StampedTransform(
 //                        tf::Transform(tf::Quaternion(0, 0, 1, 0), tf::Vector3(-0.0145, -0.0765, -0.06)),
 //                        tf::Transform(tf::Quaternion(0, 0, 0.7071, 0.7071), tf::Vector3(-0.021, 0.0215, -0.06)),
-                        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0865, 0.044, -0.06)),
+//                        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0865, 0.044, -0.06)),
+                        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1015, -0.054, -0.06)),
+
                         ros::Time::now(),"odom", "base_link"));
         broadcaster.sendTransform(
                 tf::StampedTransform(
+//                        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.0435, 0, -0.10)),
                         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.0435, 0, -0.10)),
+
                         ros::Time::now(),"base_laser", "base_link"));
 
 
